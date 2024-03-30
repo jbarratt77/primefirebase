2714dbe2c2f8f91aa4d7c09728b7c6340b7ed76e
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2024-03-30 21:09:37 +0000

    Move minutes to another screen

--- components/Meetings.tsx
+++ components/Meetings.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import {View, Text, FlatList, TouchableOpacity, StyleSheet} from 'react-native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {useNavigation} from '@react-navigation/native';
-import {useLodgeData} from '../context/LodgeDataContext';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useNavigation } from '@react-navigation/native';
+import { useLodgeData } from '../context/LodgeDataContext';
 
 const Stack = createNativeStackNavigator();
 
-function MeetingCard({meeting}) {
+function MeetingCard({ meeting }) {
   const navigation = useNavigation();
 
   const navigateToMeetingDetail = () => {
     // Navigate to Meeting Detail page with the specific meeting data
-    navigation.navigate('Meeting Detail', {meeting});
+    navigation.navigate('Meeting Detail', { meeting });
   };
 
   return (
@@ -27,14 +27,15 @@ function Meetings() {
     <Stack.Navigator>
       <Stack.Screen name="Meetings List" component={MeetingsScreen} />
       <Stack.Screen name="Meeting Detail" component={MeetingDetailScreen} />
+      <Stack.Screen name="Minutes Detail" component={MinutesDetailScreen} />
     </Stack.Navigator>
   );
 }
 
 function MeetingsScreen() {
-  const {meetingData} = useLodgeData();
+  const { meetingData } = useLodgeData();
 
-  const renderItem = ({item}) => <MeetingCard meeting={item} />;
+  const renderItem = ({ item }) => <MeetingCard meeting={item} />;
   return (
     <View style={styles.container}>
       <FlatList
@@ -47,40 +48,47 @@ function MeetingsScreen() {
   );
 }
 
-function MeetingDetailScreen({route}) {
-  const {meeting} = route.params;
+function MeetingDetailScreen({ route, navigation }) {
+  const { meeting } = route.params;
+
+  const navigateToMinutesDetail = () => {
+    // Navigate to Minutes Detail page with the specific meeting data
+    navigation.navigate('Minutes Detail', { meeting });
+  };
 
   return (
-    <>
-      <View style={styles.container}>
-        <Text style={styles.name}>{meeting.name}</Text>
-        <Text style={styles.date}>
-          {meeting.date.toDate().toLocaleString()}
-        </Text>
-        <Text style={styles.date}>
-          {meeting.location}
-        </Text>
-        <Text style={styles.sectionTitle}>Agenda:</Text>
-        <FlatList
-          data={meeting.agenda}
-          renderItem={({item}) => <Text style={styles.agendaItem}>{item}</Text>}
-          // keyExtractor={(item, index) => index.toString()}
-        />
-      </View>
-      <View style={styles.container}>
-        <Text style={styles.name}>Minutes</Text>
-        <FlatList
-          data={meeting.minutes}
-          renderItem={({item, index}) => (
-            <>
-              <Text style={styles.sectionTitle}>{meeting.agenda[index]}</Text>
-              <Text style={styles.agendaItem}>{item}</Text>
-            </>
-          )}
-          // keyExtractor={(item, index) => index.toString()}
-        />
-      </View>
-    </>
+    <View style={styles.container}>
+      <Text style={styles.name}>{meeting.name}</Text>
+      <Text style={styles.date}>{meeting.date.toDate().toLocaleString()}</Text>
+      <Text style={styles.date}>{meeting.location}</Text>
+      <Text style={styles.sectionTitle}>Agenda:</Text>
+      <FlatList
+        data={meeting.agenda}
+        renderItem={({ item }) => <Text style={styles.agendaItem}>{item}</Text>}
+      />
+      {meeting.minutes && <TouchableOpacity onPress={navigateToMinutesDetail} style={styles.minutesButton}>
+        <Text style={styles.minutesButtonText}>View Minutes</Text>
+      </TouchableOpacity>}
+    </View>
+  );
+}
+
+function MinutesDetailScreen({ route }) {
+  const { meeting } = route.params;
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.name}>Minutes</Text>
+      <FlatList
+        data={meeting.minutes}
+        renderItem={({item, index}) => (
+          <>
+            <Text style={styles.sectionTitle}>{meeting.agenda[index]}</Text>
+            <Text style={styles.agendaItem}>{item}</Text>
+          </>
+        )}
+      />
+    </View>
   );
 }
 
@@ -114,6 +122,24 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
   agendaItem: {
+    fontSize: 14,
+    marginLeft: 10,
+  },
+  minutesButton: {
+    backgroundColor: '#007AFF',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  minutesButtonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  minutesItem: {
     fontSize: 14,
     marginLeft: 10,
   },
