d482eb5348b637554b6eba4390107fb642debbf1
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2024-03-30 16:13:54 +0000

    Add nested tab nav

--- App.tsx
+++ App.tsx
@@ -1,43 +1,76 @@
 import React from 'react';
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import {Button, View, Text} from 'react-native';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import { useFirebaseAuth } from './context/FirebaseAuthContext';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {useFirebaseAuth} from './context/FirebaseAuthContext';
 import LoggedInAndMember from './components/section/LoggedInAndMember';
 import LoggedInNotMember from './components/section/LoggedInNotMember';
 import LoggedOut from './components/section/LoggedOut';
 import SplashScreen from './components/section/SplashScreen';
 
 GoogleSignin.configure({
-  webClientId: '1051714563641-admse8p2sl1n0js0oc51h5lr7vg88aan.apps.googleusercontent.com',
+  webClientId:
+    '1051714563641-admse8p2sl1n0js0oc51h5lr7vg88aan.apps.googleusercontent.com',
 });
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function App(): React.JSX.Element {
-  const { user, firestoreUser, isLoading } = useFirebaseAuth();
-  console.log(user)
+  const {user, firestoreUser, isLoading} = useFirebaseAuth();
+  console.log(user);
 
-  if(isLoading) {
-    return <SplashScreen />
+  if (isLoading) {
+    return <SplashScreen />;
+  }
+
+  function Home() {
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Home Screen</Text>
+      </View>
+    );
+  }
+
+  function Meetings() {
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Meetings Screen</Text>
+      </View>
+    );
   }
 
   return (
     <NavigationContainer>
       <Stack.Navigator>
-          {user && firestoreUser ? (
-            <>
-              <Stack.Screen name="Logged in and member" component={LoggedInAndMember}/>
-            </>
-          ) : user && !firestoreUser ? (
-            <>
-              <Stack.Screen name="Logged in not member" component={LoggedInNotMember}/>
-            </>
-          ) : (
-            <>
-              <Stack.Screen name="Logged out" component={LoggedOut}/>
-            </>
-          )}
+        {user && firestoreUser ? (
+          <>
+            <Stack.Screen
+              name="Logged in and member"
+              options={{headerShown: false}}>
+              {() => (
+                <Tab.Navigator>
+                  <Tab.Screen name="St. Erkenwald" component={Home} />
+                  <Tab.Screen name="Meetings" component={Meetings} />
+                  <Tab.Screen name="Profile" component={LoggedInAndMember} />
+                </Tab.Navigator>
+              )}
+            </Stack.Screen>
+          </>
+        ) : user && !firestoreUser ? (
+          <>
+            <Stack.Screen
+              name="Logged in not member"
+              component={LoggedInNotMember}
+            />
+          </>
+        ) : (
+          <>
+            <Stack.Screen name="Logged out" component={LoggedOut} />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
